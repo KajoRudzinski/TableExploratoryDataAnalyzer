211e597a886aafe23197a27ba2f06374e5217566
Kajo Rudzinski <Kajo.Rudzinski@zooplus.com>
2020-06-25 16:29:36 +0200

    gui improvements

--- src/gui.py
+++ src/gui.py
@@ -21,13 +21,23 @@ class GUI(Tk):
         self.title("Table Exploratory Data Analyzer")
         self.minsize(640, 400)
 
+        self.app_status = StringVar()
+
         # self.wm_iconbitmap('icon.ico')
         self.labelFrame_SelectFile = None
+        self.labelFrame_Status = None
         self.button_SelectFile = None
+        
+        # To be changed to set appropriate column and row weight and padding
+        # self.columnconfigure(1, weight=1)
+        # self.columnconfigure(3, pad=7)
+        # self.rowconfigure(3, weight=1)
+        # self.rowconfigure(5, pad=7)
 
         self.setup_gui_style()
-        self.create_labelFrame_SelectFile()
-        self.create_button_SelectFile()
+        self.create_labelFrame_selectFile()
+        self.create_label_status()
+        self.create_button_selectFile()
 
         self.selected_filePath = None
 
@@ -35,11 +45,15 @@ class GUI(Tk):
     def setup_gui_style():
         ttk.Style().configure("TButton", padding=6, relief="flat", background="#ccc")
 
-    def create_labelFrame_SelectFile(self):
+    def create_labelFrame_selectFile(self):
         self.labelFrame_SelectFile = ttk.LabelFrame(self, text="Open File")
-        self.labelFrame_SelectFile.grid(column=0, row=1, padx=20, pady=20)
+        self.labelFrame_SelectFile.grid(column=0, row=1)
 
-    def create_button_SelectFile(self):
+    def create_label_status(self):
+        self.labelFrame_Status = ttk.Label(self, text="welcome", relief="sunken")
+        self.labelFrame_Status.grid(column=2, row=1, padx=10)
+
+    def create_button_selectFile(self):
         self.button_SelectFile = ttk.Button(self.labelFrame_SelectFile, text="Browse A File", command=self.fileDialog)
         self.button_SelectFile.grid(column=1, row=1)
 
@@ -53,3 +67,4 @@ class GUI(Tk):
 def run_gui():
     gui = GUI()
     gui.mainloop()
+
