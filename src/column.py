69c4652e5b79c0ee382f637ffa2ce96b2873ac92
KajoRudzinski <kajo.rudzinski@gmail.com>
2020-07-15 23:05:04 +0200

    group by bins issue and top n issue

--- src/column.py
+++ src/column.py
@@ -33,20 +33,25 @@ class Dimension(Column):
     def __init__(self, column: pd.Series):
         super().__init__(column)
         self.data_filled_na: pd.Series = column.fillna(value="_None_")
+        self.groups_allowed = 20
 
-    def group_by_distinct(self, rel_or_abs="abs", top_n=None) -> dict:
+    def group_by_distinct(
+            self, relative: bool = False, top_n: int = None) -> dict:
         if top_n is None:
-            return self._group_by(rel_or_abs=rel_or_abs)
+            return self._group_by(relative=relative)
         else:
-            return self._group_by_top_n(rel_or_abs, top_n)
+            return self._group_by_top_n(relative=relative, top_n=top_n)
 
-    def _group_by_top_n(self, rel_or_abs, top_n):
-        d = self._group_by(rel_or_abs=rel_or_abs)
+    def _group_by_top_n(self, relative: bool, top_n: int):
+        d = self._group_by(relative=relative)
         return hq.nlargest(top_n, d, key=d.get)
 
-    def _group_by(self, rel_or_abs="abs"):
-        if rel_or_abs == "abs":
-            return self.data_filled_na.value_counts()
-        if rel_or_abs == "rel":
-            return self.data_filled_na.value_counts(normalize=True)
-
+    def _group_by(self, relative: bool):
+        if relative is True:
+            groups = self.data_filled_na.value_counts(normalize=True)
+        else:
+            groups = self.data_filled_na.value_counts()
+        if groups.len() > self.groups_allowed:
+            return groups
+        else:
+            return {"Many values": 1}
